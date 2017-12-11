853d82e1b885e0e5864b966c8c98efce4a0f3aa3
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-11 12:19:49 -0600

    Using var and val.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,5 +1,25 @@
 package learnprogrammingacademy.learning
 
+var lives = 3
+var playerName = "John"
+var gameOver = false
+
+val levelCount = 4
+
 fun main(args: Array<String>) {
-    println("Hello Kotlin")
+    printLivesLeft()
+
+    lives += 2
+    playerName += " " + "Smith"
+
+    printLivesLeft()
+    println("gameOver = $gameOver")
+
+    println("PlayerName $playerName needs to comple $levelCount leels")
+    gameOver = true
+    println("gameOver = $gameOver")
+}
+
+private fun printLivesLeft(){
+    println("Player $playerName has $lives lives left")
 }
