bce64e4d1c3ae027c29181ed18dd8bf6f063ac51
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-11 14:50:17 -0600

    String and his its methods.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,55 +1,37 @@
 package learnprogrammingacademy.learning
 
 fun main(args: Array<String>) {
-    val animals = arrayOf("Dog", "Cat", "Lizard", "Bird", "Snake")
-    breakExample(animals)
-    continueExample(animals)
-}
+    val dog = "Dog"
+    val dog2 = String(charArrayOf('D', 'o', 'g'))
 
-private fun breakExample(animals: Array<String>) {
-    breakForExample(animals)
-    breakLambdaExample(animals)
-}
+    println("dog == dog2 => ${dog == dog2}")//Structural equality (Check the content)
+    println("dog === dog2 => ${dog === dog2}")//Reference equality
 
-@Suppress("LoopToCallChain")
-private fun breakForExample(animals: Array<String>){
-    println("*breakForExample")
-    for(animal in animals){
-        if(animal == "Lizard"){
-            break
-        }
+    val cat = "Cat"
+    val cat2 = String(cat.toCharArray())
+    val cat3 = cat2//Two referenes to sae object int memory
 
-        println(animal)
-    }
-}
+    println("reference equals => ${cat === cat2}")
+    println("reference equals => ${cat === cat3}")
+    println("reference equals => ${cat2 === cat3}")
 
-private fun breakLambdaExample(animals: Array<String>){
-    println("*breakLambdaExample")
-    animals.asSequence()
-            .takeWhile { it != "Lizard" }
-            .forEach { println(it) }
-}
+    //Templates
+    println("Dog= $dog Cat= $cat")
 
-private fun continueExample(animals: Array<String>){
-    continueForExample(animals)
-    continueLambdaExample(animals)
-}
+    //Raw
+    println("""This is
+my dog
+$dog""")
 
-@Suppress("LoopToCallChain")
-private fun continueForExample(animals: Array<String>){
-    println("*continueForExample")
-    for(animal in animals){
-        if(animal == "Lizard"){
-            continue
-        }
+    //Methods
+    var lizard = "Lizard"
+    println("Lizard first letter= ${lizard[0]}")
+    println("contains= ${lizard.contains("iz")}")
 
-        println(animal)
-    }
-}
+    //Immutability
+    lizard.toUpperCase() //It creates copy or new string object
+    println("Lizard= $lizard")
 
-private fun continueLambdaExample(animals: Array<String>){
-    println("*continueLambdaExample")
-    animals.asSequence()
-            .filter { it != "Lizard" }
-            .forEach { println(it) }
+    lizard = lizard.toUpperCase()
+    println("Lizard= $lizard")
 }
