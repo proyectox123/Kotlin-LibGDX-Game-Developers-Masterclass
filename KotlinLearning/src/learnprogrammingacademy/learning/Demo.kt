b5ba9ded4560d83e7e9767a81fe10a44ac6237c4
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-11 14:33:21 -0600

    Break and continue.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,50 +1,55 @@
 package learnprogrammingacademy.learning
 
 fun main(args: Array<String>) {
-    /*
-    Use any loop from previous videos (for, while, do-while)
-    sum all number from 1-100
-    print sum to console
-     */
-    printWithFor()
-    printWithSum()
-    printWithWhile()
-    printWithDoWhile()
+    val animals = arrayOf("Dog", "Cat", "Lizard", "Bird", "Snake")
+    breakExample(animals)
+    continueExample(animals)
+}
+
+private fun breakExample(animals: Array<String>) {
+    breakForExample(animals)
+    breakLambdaExample(animals)
 }
 
 @Suppress("LoopToCallChain")
-private fun printWithFor(){
-    var sum = 0
-    for(i in 1..100){
-        sum += i
+private fun breakForExample(animals: Array<String>){
+    println("*breakForExample")
+    for(animal in animals){
+        if(animal == "Lizard"){
+            break
+        }
+
+        println(animal)
     }
-
-    println("printWithFor sum= $sum")
 }
 
-private fun printWithSum(){
-    val sum = (1..100).sum()
-    println("printWithSum sum= $sum")
+private fun breakLambdaExample(animals: Array<String>){
+    println("*breakLambdaExample")
+    animals.asSequence()
+            .takeWhile { it != "Lizard" }
+            .forEach { println(it) }
 }
 
-private fun printWithWhile(){
-    var sum = 0
-    var index = 100
-    while (index > 0){
-        sum += index
-        index--
+private fun continueExample(animals: Array<String>){
+    continueForExample(animals)
+    continueLambdaExample(animals)
+}
+
+@Suppress("LoopToCallChain")
+private fun continueForExample(animals: Array<String>){
+    println("*continueForExample")
+    for(animal in animals){
+        if(animal == "Lizard"){
+            continue
+        }
+
+        println(animal)
     }
-
-    println("printWithWhile sum= $sum")
 }
 
-private fun printWithDoWhile(){
-    var sum = 0
-    var index = 1
-    do{
-        sum += index
-        index++
-    }while (index <= 100)
-
-    println("printWithDoWhile sum= $sum")
+private fun continueLambdaExample(animals: Array<String>){
+    println("*continueLambdaExample")
+    animals.asSequence()
+            .filter { it != "Lizard" }
+            .forEach { println(it) }
 }
