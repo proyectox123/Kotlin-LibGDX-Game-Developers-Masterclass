9e22cbcebac653ec3a0d8818f8241c6c35a4de1b
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-11 15:04:39 -0600

    String challenge.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,37 +1,19 @@
 package learnprogrammingacademy.learning
 
 fun main(args: Array<String>) {
-    val dog = "Dog"
-    val dog2 = String(charArrayOf('D', 'o', 'g'))
+    /*
+    Create array of strings that represents names of people
+    Print all string names that start with capital letter T in uppercase
+    Find method in String class that checks if staring startsWith another string
+     */
 
-    println("dog == dog2 => ${dog == dog2}")//Structural equality (Check the content)
-    println("dog === dog2 => ${dog === dog2}")//Reference equality
+    //val names = arrayOf("Tania", "Ximena", "Tadeo", "Víctor", "Marcos")
+    val names = arrayOf("John", "Tom", "Anthony", "Jimmy", "Timmy")
+    printNames(names)
+}
 
-    val cat = "Cat"
-    val cat2 = String(cat.toCharArray())
-    val cat3 = cat2//Two referenes to sae object int memory
-
-    println("reference equals => ${cat === cat2}")
-    println("reference equals => ${cat === cat3}")
-    println("reference equals => ${cat2 === cat3}")
-
-    //Templates
-    println("Dog= $dog Cat= $cat")
-
-    //Raw
-    println("""This is
-my dog
-$dog""")
-
-    //Methods
-    var lizard = "Lizard"
-    println("Lizard first letter= ${lizard[0]}")
-    println("contains= ${lizard.contains("iz")}")
-
-    //Immutability
-    lizard.toUpperCase() //It creates copy or new string object
-    println("Lizard= $lizard")
-
-    lizard = lizard.toUpperCase()
-    println("Lizard= $lizard")
+private fun printNames(names: Array<String>) {
+    names.asSequence()
+            .filter { it.startsWith("T") }
+            .forEach { println("Name= ${it.toUpperCase()}") }
 }
