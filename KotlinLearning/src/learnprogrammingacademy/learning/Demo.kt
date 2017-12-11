47579562601a6333d9b127e4bf7cdd10458d1c01
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-11 13:37:20 -0600

    Do and do-while loops.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,45 +1,26 @@
 package learnprogrammingacademy.learning
 
 var lives = 3
-var score = 0
-var coinCount = 110
-var gameOver = false
+var coinCount = 90
 
 fun main(args: Array<String>) {
-    score += 10
-    coinCount = 110 //Re-initializing
-    //lives = 0
+    whileFunction()
+    lives = 3
+    doWhileFunction()
+}
 
-    if(lives == 0){
-        gameOver = true
-    }
-
-    printMedal()
-
-    if(gameOver){
-       println("score= $score")
-    }else{
+private fun whileFunction(){
+    while (lives > 0) {
         println("lives= $lives")
-    }
-
-    if(!gameOver){
-        println("Game is not over")
-    }
-
-    val max = getMax(5, 8)
-    println("Max= $max")
-}
-
-private fun printMedal(){
-    if(coinCount > 100){
-        println("Gold medal!")
-    } else if(coinCount > 80){
-        println("Silver medal!")
-    } else if(coinCount > 70){
-        println("Bronze medal!")
-    } else {
-        println("No medal...")
+        lives--
     }
 }
 
-private fun getMax(a: Int, b: Int) = if(a > b) a else b
+private fun doWhileFunction(){
+    do {
+        coinCount += 20
+        lives--
+    }while (lives > 0)
+
+    println("coins= $coinCount lives= $lives")
+}
