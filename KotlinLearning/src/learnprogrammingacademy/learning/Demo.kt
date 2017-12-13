e5e349b4fb7218790226155ea34f3e8ff4170ba4
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-13 14:10:41 -0600

    Static methods and function extensions (including infix).

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,70 +1,27 @@
 package learnprogrammingacademy.learning
 
-/*
-Write class that represents a person
-Person has 3 properties/fields: firstName, lastName, age
-Write setter/set block for age to limit age between 0 and 100
-Use init block to initialize age
-Create read only property fullName (firstName + lastName)
-In main create instance of person
-Print firstName, last, fullName and age
-Tip: Create method in person class to print properties
- */
-
-class Person(private var firstName: String, private var lastName: String, age: Int){
-
-    var age: Int = 0
-        set(value){
-            field = when {
-                value < 0 -> 0
-                value > 100 -> 100
-                else -> value
-            }
-        }
-
-    private val fullName: String
-        get() = "$firstName $lastName"
-
-    init {
-        this.age = age
-    }
-
-    fun printInfo() = println("firstName= $firstName lastName= $lastName fullName= $fullName age= $age")
+object StringUtils {
+    fun startsWithUpperCase(value: String?) : Boolean = !(value.isNullOrEmpty()) && value!![0].isUpperCase()
 }
+
+fun String?.startsWithUpperCase() : Boolean = !(this.isNullOrEmpty()) && this!![0].isUpperCase()
+
+infix fun Int.multiplyBy(multiplier: Int) = this * multiplier
 
 fun main(args: Array<String>) {
-    val person1 = Person("John", "Smith", 25)
-    person1.printInfo()
+    val myString = "Kotlin"
 
-    val person2 = Person("Jimmy", "Winter", 120)
-    person2.printInfo()
+    println("toLower=  ${myString.toLowerCase()}")
+    println("startsWithUpperCase= ${StringUtils.startsWithUpperCase(myString)}")
+    println("startsWithUpperCase= ${StringUtils.startsWithUpperCase(null)}")
+    println("startsWithUpperCase= ${StringUtils.startsWithUpperCase("")}")
+    println("startsWithUpperCase= ${StringUtils.startsWithUpperCase(myString.toLowerCase())}")
 
-    val person3 = Person("Anthony", "Bell", -10)
-    person3.printInfo()
+    println("startsWithUpperCase ext= ${myString.startsWithUpperCase()}")
+    println("startsWithUpperCase ext= ${null.startsWithUpperCase()}")
+    println("startsWithUpperCase ext= ${"".startsWithUpperCase()}")
+    println("startsWithUpperCase ext= ${myString.toLowerCase().startsWithUpperCase()}")
+
+    println("1*5 ext= ${1.multiplyBy(5)}")
+    println("1*5 ext= ${1 multiplyBy 5}")
 }
-
-/*
-class Person(private val firstName: String, private val lastName: String){
-
-    var age : Int = 0
-        set(value) {
-            if(value < 0 || value > 100){
-                throw Exception("Age is not valid")
-            }
-
-            field = value
-        }
-    private val fullName: String
-        get() {
-            return "$firstName $lastName"
-        }
-
-    fun printInfo() = println("firstName= $firstName\nlastName= $lastName\nfullName=$fullName\nage=$age")
-}
-
-fun main(args: Array<String>) {
-    val person = Person("Jimmy", "Jones")
-    person.age = 12
-    person.printInfo()
-}
- */
