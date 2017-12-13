6b988e716ee2fe61b12435d0a8155973eb47c525
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-13 15:06:30 -0600

    Inheritance example (Pikeman and Archer).

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,36 +1,31 @@
 package learnprogrammingacademy.learning
 
-/*
-Write extension method that will return true if string has even number of characters or false if string has odd number of characters
-Test method in main with few different strings
-Write method in StringUtils that will calculate sum of two strings lengths, or in other words sum of characters in 2 strings
-Tip: Use modulus operator for even odd testing
-Tip: Be careful with null values and empty strings
- */
+open class Enemy(var health: Int, var weapon: String){
+    init {
+        println("Enemy init called")
+    }
 
-fun String?.hasEvenNumberOfCharacters() = this != null && this.length % 2 == 0
+    fun attack(){
+        println("Attacking with $weapon")
+    }
+}
 
-object StringUtils {
-    fun sumStringsLengths(param1: String?, param2: String?) = getStringLength(param1) + getStringLength(param2)
+class Pikeman(health: Int, var armor: Int) : Enemy(health, "pike"){
+    init {
+        println("Pikeman init called")
+    }
+}
 
-    private fun getStringLength(param: String?) = if(param.isNullOrBlank()){
-        0
-    }else{
-        param!!.length
+class Archer(health: Int, var arrowCount: Int) : Enemy(health, "bow"){
+    init {
+        println("Archer init called")
     }
 }
 
 fun main(args: Array<String>) {
-    println("hasEvenNumberOfCharacters= ${null.hasEvenNumberOfCharacters()}")
-    println("hasEvenNumberOfCharacters= ${"".hasEvenNumberOfCharacters()}")
-    println("hasEvenNumberOfCharacters= ${"Kotlin".hasEvenNumberOfCharacters()}")
-    println("hasEvenNumberOfCharacters= ${"Kotlin1".hasEvenNumberOfCharacters()}")
+    val pikeman = Pikeman(100, 100)
+    pikeman.attack()
 
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths(null, null)}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths(null, "")}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths(null, "World!")}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths("", null)}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths("", "World!")}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths("Hello", null)}")
-    println("sumStringsLengths= ${StringUtils.sumStringsLengths("Hello", "World!")}")
+    val archer = Archer(100, 5)
+    archer.attack()
 }
