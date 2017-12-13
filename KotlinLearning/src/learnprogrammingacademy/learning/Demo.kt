cf9a070473597ffd93fb142c554d26e7332482f8
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-13 11:24:46 -0600

    Named and optional parameters.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,28 +1,17 @@
 package learnprogrammingacademy.learning
 
 fun main(args: Array<String>) {
-    /*
-    Write 3 methods with name sum
-    First method needs to sum 2 numbers
-    First method needs to sum 3 numbers
-    First method needs to sum 4 numbers
-    Call all 3 methods and print result to console
-    All parameters and return types can be int
-     */
-    val a = 1
-    val b = 2
-    val c = 3
+    showInfo()
+    showInfo("Jimmy")
+    showInfo("Anthony", 10)
+    showInfo(level = 5, name = "Timmy")
+    showInfo(level = 15)
 
-    var sum = sum(a, b)
-    println("Sum(a, b)= $sum")
-
-    sum = sum(a, b, sum)
-    println("Sum(a, b, c)= $sum")
-
-    sum = sum(a, b, c, sum)
-    println("Sum(a, b, c, d)= $sum}")
+    showLevelAndXp(5, 1000)
+    showLevelAndXp(xp = 10, level = 3)
+    showLevelAndXp(level = 10, xp = 5000)
 }
 
-private fun sum(a: Int, b: Int) = a + b
-private fun sum(a: Int, b: Int, c: Int) = a + b + c
-private fun sum(a: Int, b: Int, c: Int, d:Int) = a + b + c + d
+private fun showInfo(name: String = "John", level: Int = 2) = println("name= $name level= $level")
+
+private fun showLevelAndXp(level: Int, xp: Int) = println("level= $level xp= $xp")
