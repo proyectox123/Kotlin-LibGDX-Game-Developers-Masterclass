2cd4ea111eeee6764652eda069360b237e33217c
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-13 13:45:36 -0600

    Encapsulation challenge.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,42 +1,70 @@
 package learnprogrammingacademy.learning
 
-class Ship {
-    private val bullets = arrayOf("laser", "fire")
+/*
+Write class that represents a person
+Person has 3 properties/fields: firstName, lastName, age
+Write setter/set block for age to limit age between 0 and 100
+Use init block to initialize age
+Create read only property fullName (firstName + lastName)
+In main create instance of person
+Print firstName, last, fullName and age
+Tip: Create method in person class to print properties
+ */
 
-    var hitPoints = 100
-        private set
+class Person(private var firstName: String, private var lastName: String, age: Int){
 
-    fun fire(){
-        val bullet = if(hitPoints > 50){
-            bullets[1]
-        }else{
-            bullets[0]
-        }
-
-        println("Firing $bullet")
-    }
-
-    fun takeDamage(amount: Int){
-        if(amount > 0){
-            hitPoints -= amount
-            if(hitPoints < 0){
-                hitPoints = 0
+    var age: Int = 0
+        set(value){
+            field = when {
+                value < 0 -> 0
+                value > 100 -> 100
+                else -> value
             }
         }
+
+    private val fullName: String
+        get() = "$firstName $lastName"
+
+    init {
+        this.age = age
     }
+
+    fun printInfo() = println("firstName= $firstName lastName= $lastName fullName= $fullName age= $age")
 }
 
 fun main(args: Array<String>) {
-    val ship = Ship()
-    ship.fire()
-    ship.takeDamage(10)
-    println("ship hitPoints= ${ship.hitPoints}")
+    val person1 = Person("John", "Smith", 25)
+    person1.printInfo()
 
-    ship.fire()
-    ship.takeDamage(50)
-    println("ship hitPoints= ${ship.hitPoints}")
+    val person2 = Person("Jimmy", "Winter", 120)
+    person2.printInfo()
 
-    ship.fire()
-    ship.takeDamage(100)
-    println("ship hitPoints= ${ship.hitPoints}")
+    val person3 = Person("Anthony", "Bell", -10)
+    person3.printInfo()
 }
+
+/*
+class Person(private val firstName: String, private val lastName: String){
+
+    var age : Int = 0
+        set(value) {
+            if(value < 0 || value > 100){
+                throw Exception("Age is not valid")
+            }
+
+            field = value
+        }
+    private val fullName: String
+        get() {
+            return "$firstName $lastName"
+        }
+
+    fun printInfo() = println("firstName= $firstName\nlastName= $lastName\nfullName=$fullName\nage=$age")
+}
+
+fun main(args: Array<String>) {
+    val person = Person("Jimmy", "Jones")
+    person.age = 12
+    person.printInfo()
+}
+ */
