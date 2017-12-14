9aa04bc7149ae1f87cbae9dd664d2a28daa7478c
MHO <mauricio.hdez.o.10@gmail.com>
2017-12-13 19:05:34 -0600

    Implemented Armor class.

--- KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
+++ KotlinLearning/src/learnprogrammingacademy/learning/Demo.kt
@@ -1,25 +1,37 @@
 package learnprogrammingacademy.learning
 
+/*
+Write class Armor similar to Health
+Create property armor in Soldier class
+When printing soldier, make sure it prints armor (modify toString method)
+Add armor to Pikeman and archer inside main method
+Create swordsman soldier and print to console
+ */
+
+class Armor(var amount: Int)
+
 class Health(var amount: Int)
 
 class Weapon(var name: String)
 
-class Soldier(var name: String, var health: Health, var weapon: Weapon){
-    override fun toString() = "name='$name', health=${health.amount}, weapon=${weapon.name}"
+class Soldier(var name: String, var health: Health, var weapon: Weapon, var armor: Armor){
+    override fun toString() = "name='$name', health=${health.amount}, weapon=${weapon.name}, armor=${armor.amount}"
 }
 
 fun main(args: Array<String>) {
     //Pikeman
     val pikemanHealth= Health(100)
     val pike = Weapon("pike")
-    val pikeman = Soldier("pikeman", pikemanHealth, pike)
+    val pikemanArmor = Armor(100)
+    val pikeman = Soldier("pikeman", pikemanHealth, pike, pikemanArmor)
 
     println("pikeman= $pikeman")
 
     //Archer
     val archerHealth= Health(80)
     val bow = Weapon("bow")
-    val archer = Soldier("archer", archerHealth, bow)
+    val archerArmor = Armor(80)
+    val archer = Soldier("archer", archerHealth, bow, archerArmor)
 
     println("archer= $archer")
 }
